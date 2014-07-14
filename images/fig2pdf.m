941959bd3bb620e0d57867f5707b85352c3c4ae3
David Love <love.david.k@gmail.com>
2014-07-13 20:45:11 -0400

    Fix right hand side of all images -- some were missing the axis

--- images/fig2pdf.m
+++ images/fig2pdf.m
@@ -22,7 +22,7 @@ for j = 1:length(s)
     pos = get(gca,'Position');
     ti = get(gca,'TightInset');
     set(gcf, 'PaperUnits','centimeters');
-    set(gcf, 'PaperSize', [pos(3)+ti(1)+ti(3) pos(4)+ti(2)+ti(4)]);
+    set(gcf, 'PaperSize', [(pos(3)+ti(1)+ti(3))*1.05 (pos(4)+ti(2)+ti(4))*1.05]);
     set(gcf, 'PaperPositionMode', 'manual');
     set(gcf, 'PaperPosition',[0 0 pos(3)+ti(1)+ti(3) pos(4)+ti(2)+ti(4)]);
     
